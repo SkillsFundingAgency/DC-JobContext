84ef76987f6560e2abcb82b0a3882e322271dc8f
John Youren <john.youren@gmail.com>
2018-05-11 15:16:37 +0100

    Added keys for the counts of the valid and invalid learners

--- src/ESFA.DC.JobContext.Interface/JobContextMessageKey.cs
+++ src/ESFA.DC.JobContext.Interface/JobContextMessageKey.cs
@@ -2,12 +2,49 @@
 {
     public enum JobContextMessageKey
     {
+        /// <summary>
+        /// The UkPrn number. (int)
+        /// </summary>
         UkPrn,
+
+        /// <summary>
+        /// The name of the container containing the file to process. (string)
+        /// </summary>
         Container,
+
+        /// <summary>
+        /// The name of the file to process. (string)
+        /// </summary>
         Filename,
+
+        /// <summary>
+        /// The size of the file in bytes. (long)
+        /// </summary>
         FileSizeInBytes,
+
+        /// <summary>
+        /// The username who started the operation. (string)
+        /// </summary>
         Username,
+
+        /// <summary>
+        /// The *Key* of the serialised valid learn ref numbers in storage. (string)
+        /// </summary>
         ValidLearnRefNumbers,
-        InvalidLearnRefNumbers
+
+        /// <summary>
+        /// The number of valid learners. (long)
+        /// </summary>
+        ValidLearnRefNumbersCount,
+
+        /// <summary>
+        /// The *Key* of the serialised invalid learn ref numbers in storage. (string)
+        /// </summary>
+        InvalidLearnRefNumbers,
+
+        /// <summary>
+        /// The number of invalid learners. (long)
+        /// </summary>
+        InvalidLearnRefNumbersCount
     }
 }
